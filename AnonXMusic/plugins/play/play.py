0b1493a31386e08dd3264b778286e869bf8b79bd
HààMódY <143948816+byemuhvmed@users.noreply.github.com>
2024-03-26 20:33:46 +0200

    Update play.py

--- AnonXMusic/plugins/play/play.py
+++ AnonXMusic/plugins/play/play.py
@@ -28,7 +28,7 @@ from config import BANNED_USERS, lyrical
 @app.on_message(
     filters.command(
         [
-            "play",
+            "تشغيل",
             "vplay",
             "cplay",
             "cvplay",
