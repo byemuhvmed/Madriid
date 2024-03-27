b540e17cd732d053f4be20705ccb37c653b56a5a
HààMódY <143948816+byemuhvmed@users.noreply.github.com>
2024-03-28 00:30:40 +0200

    Update play.py

--- AnonXMusic/plugins/play/play.py
+++ AnonXMusic/plugins/play/play.py
@@ -25,7 +25,7 @@ from AnonXMusic.utils.stream.stream import stream
 from config import BANNED_USERS, lyrical
 
 
-@app.on_message(filters.command(["تشغيل","فيديو"], "")
+@app.on_message(filters.command(["تشغيل","شغل"], "")
 )
 @app.on_message(
     filters.command( 
