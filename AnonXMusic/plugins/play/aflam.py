5da05e81c1a7e02935d3662a7d768e70548f8e22
HààMódY <143948816+byemuhvmed@users.noreply.github.com>
2024-03-31 08:28:54 +0200

    Update aflam.py

--- AnonXMusic/plugins/play/aflam.py
+++ AnonXMusic/plugins/play/aflam.py
@@ -1,61 +1,39 @@
 import asyncio
+
 import os
 import time
 import requests
-import aiohttp
+from config import START_IMG_URL
 from pyrogram import filters
+import random
 from pyrogram import Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
 from strings.filters import command
-from AnonXMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
-from AnonXMusic import app
-from asyncio import gather
+from ZeMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from ZeMusic import app
+from random import  choice, randint
 
-
-@app.on_message(command(["تلغراف", "تلغراف ميديا", "تليجراف"]) & ~filters.edited)
-async def telegraph(client: Client, message: Message):
-    replied = message.reply_to_message
-    if not replied:
-        await message.reply("🤕 ¦ الرد على ملف وسائط مدعوم\n• حط صوره و اكتب عليها 00")
-        return
-    if not (
-        (replied.photo and replied.photo.file_size <= 5242880)
-        or (replied.animation and replied.animation.file_size <= 5242880)
-        or (
-            replied.video
-            and replied.video.file_name.endswith(".mp4")
-            and replied.video.file_size <= 5242880
-        )
-        or (
-            replied.document
-            and replied.document.file_name.endswith(
-                (".jpg", ".jpeg", ".png", ".gif", ".mp4"),
-            )
-            and replied.document.file_size <= 5242880
-        )
-    ):
-        await message.reply("غير مدعوم!!")
-        return
-    download_location = await client.download_media(
-        message=message.reply_to_message,
-        file_name="root/downloads/",
-    )
-    try:
-        response = upload_file(download_location)
-    except Exception as document:
-        await message.reply(message, text=document)
-    else:
-        await message.reply(
-            f"<b>• الــرابـط:-</b>\n\n <code>https://telegra.ph{response[0]}</code>",
-            quote=True,
-            reply_markup=InlineKeyboardMarkup(
+#          
+                
+@app.on_message(filters.command(["حمودي"‚"المطور"],"")
+)
+async def huhh(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/6fe7de7485c993122bc4a.jpg",
+        caption=f"""Developer Name : ˛ hamody .  
+Devloper Username : @MH_BP ، 
+‏ⓘ متسالنيش عن احوالي ، انا تايه """,
+        reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton(text="🎯 ¦ افـتح الـرابـط", url=f"https://telegra.ph{response[0]}"),
-                    InlineKeyboardButton(text="♻️ ¦ مشـاركه الـرابـط", url=f"https://telegram.me/share/url?url=https://telegra.ph{response[0]}")
+                    InlineKeyboardButton(
+                        "hamody .", url=f"https://t.me/MH_BP"), 
+                 ],[
+                   
                 ],
+
             ]
-        )
+
+        ),
+
     )
-    finally:
-        os.remove(download_location)
