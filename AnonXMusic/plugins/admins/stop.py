77efa91f076267399adf4fc0aa82a2949d762644
HààMódY <143948816+byemuhvmed@users.noreply.github.com>
2024-04-15 15:59:27 +0200

    Update stop.py

--- AnonXMusic/plugins/admins/stop.py
+++ AnonXMusic/plugins/admins/stop.py
@@ -1,24 +1,23 @@
-from pyrogram import filters 
-from pyrogram.types import Message 
- 
-from AnonXMusic import app 
-from AnonXMusic.core.call import Anony 
-from AnonXMusic.utils.database import set_loop 
-from AnonXMusic.utils.decorators import AdminRightsCheck 
-from AnonXMusic.utils.inline import close_markup 
-from config import BANNED_USERS 
-from pyrogram.enums import ChatType 
- 
- 
-@app.on_message( 
-    filters.command(["/end", "/stop", "/cend", "/cstop","انهاء","ايقاف"],"") & ~filters.private & ~BANNED_USERS 
-) 
-@AdminRightsCheck 
-async def stop_music(cli, message: Message, _, chat_id): 
-    if not len(message.command) == 1: 
-        return 
-    await Anony.stop_stream(chat_id) 
-    await set_loop(chat_id, 0) 
-    await message.reply_text( 
-        _["admin_5"].format(message.from_user.mention if message.chat.type != ChatType.CHANNEL else "مشرف القناة"), reply_markup=close_markup(_) 
+from pyrogram import filters
+from pyrogram.types import Message
+
+from AnonXMusic import app
+from AnonXMusic.core.call import Anony
+from AnonXMusic.utils.database import set_loop
+from AnonXMusic.utils.decorators import AdminRightsCheck
+from AnonXMusic.utils.inline import close_markup
+from config import BANNED_USERS
+
+
+@app.on_message(
+    filters.command(["end", "stop", "اسكت", "cstop","ايقاف","انهاء"], "") & filters.group & ~BANNED_USERS
+)
+@AdminRightsCheck
+async def stop_music(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1:
+        return
+    await Anony.stop_stream(chat_id)
+    await set_loop(chat_id, 0)
+    await message.reply_text(
+        _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
     )
