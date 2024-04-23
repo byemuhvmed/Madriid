8a7b84472436d616af2574234066418c6d0433e6
HààMódY <143948816+byemuhvmed@users.noreply.github.com>
2024-04-23 21:40:53 +0200

    Update autoend.py

--- AnonXMusic/plugins/sudo/autoend.py
+++ AnonXMusic/plugins/sudo/autoend.py
@@ -6,19 +6,19 @@ from AnonXMusic.misc import SUDOERS
 from AnonXMusic.utils.database import autoend_off, autoend_on
 
 
-@app.on_message(filters.command("autoend") & SUDOERS)
+@app.on_message(filters.command(["autoend", "الايقاف التلقائي"], "") & SUDOERS)
 async def auto_end_stream(_, message: Message):
-    usage = "<b>ᴇxᴀᴍᴘʟᴇ :</b>\n\n/autoend [ᴇɴᴀʙʟᴇ | ᴅɪsᴀʙʟᴇ]"
+    usage = "<b>مثال :</b>\n\n/autoend [enable | disable]"
     if len(message.command) != 2:
         return await message.reply_text(usage)
     state = message.text.split(None, 1)[1].strip().lower()
     if state == "enable":
         await autoend_on()
         await message.reply_text(
-            "» ᴀᴜᴛᴏ ᴇɴᴅ sᴛʀᴇᴀᴍ ᴇɴᴀʙʟᴇᴅ.\n\nᴀssɪsᴛᴀɴᴛ ᴡɪʟʟ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ʟᴇᴀᴠᴇ ᴛʜᴇ ᴠɪᴅᴇᴏᴄʜᴀᴛ ᴀғᴛᴇʀ ғᴇᴡ ᴍɪɴs ᴡʜᴇɴ ɴᴏ ᴏɴᴇ ɪs ʟɪsᴛᴇɴɪɴɢ."
+            "**تم تفعيل الايقاف التلقائي ↺**"
         )
     elif state == "disable":
         await autoend_off()
-        await message.reply_text("» ᴀᴜᴛᴏ ᴇɴᴅ sᴛʀᴇᴀᴍ ᴅɪsᴀʙʟᴇᴅ.")
+        await message.reply_text("**تم تعطيل الايقاف التلقائي ↺**")
     else:
         await message.reply_text(usage)
