e0ef476eb83af23e9542ffe1c84de8ac58c71fca
HààMódY <143948816+byemuhvmed@users.noreply.github.com>
2024-04-25 02:23:25 +0200

    Update start.py

--- AnonXMusic/plugins/bot/start.py
+++ AnonXMusic/plugins/bot/start.py
@@ -83,16 +83,22 @@ async def start_pm(client, message: Message, _):
                     text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
                 )
     else:
+        await message.reply("صلي على النبي وتبسم ♥️✨")
+        if client.me.photo:
+            async for photo in app.get_chat_photos("me",limit=1):
+                start_img = photo.file_id
+        else:
+            start_img = config.START_IMG_URL
         out = private_panel(_)
         await message.reply_photo(
-            photo=config.START_IMG_URL,
+            photo=start_img,
             caption=_["start_2"].format(message.from_user.mention, app.mention),
             reply_markup=InlineKeyboardMarkup(out),
         )
         if await is_on_off(2):
             return await app.send_message(
                 chat_id=config.LOGGER_ID,
-                text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
+                text=f"<b>• في حد دخل البوت◟</b>\n\n<b>• اسمة :{message.from_user.mention}\n<b>• اسمة :</b> <code>{message.from_user.id}</code>\n<b>• اليوزر :</b> @{message.from_user.username}",
             )
 
 
